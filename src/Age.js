55c68fbeae4220e0ed4a8dd55927dde5166d326a
Dip <dipbd1@gmail.com>
2020-08-07 10:41:23 +0600

    Fixed Year Issue

--- src/Age.js
+++ src/Age.js
@@ -8,62 +8,80 @@ import "react-datepicker/dist/react-datepicker.css";
 class Age extends Component {
 	state = {
 		startDate: new Date(),
-        currentTime: new Date(),
-        counter: true
+		currentTime: new Date(),
+		counter: true,
 	};
 
 	handleChange = (date) => {
-        clearInterval(this.timer);
+		clearInterval(this.timer);
 		this.setState({
 			startDate: date,
-            currentTime: new Date(),
-            counter : false,
-            timer : setInterval(() => {
-                this.setState({
-                    currentTime: new Date(),
-                });
-            }, 10)
-        });
-
-    };
-    
-
+			currentTime: new Date(),
+			counter: false,
+			timer: setInterval(() => {
+				this.setState({
+					currentTime: new Date(),
+				});
+			}, 10),
+		});
+	};
 
 	render() {
-        var dateWithzero = this.state.startDate.setMinutes(0);
+		var dateWithzero = this.state.startDate.setHours(0, 0, 0);
 		var time = this.state.currentTime - dateWithzero;
-        var minDate = new Date("1930-01-01");
-        
+		var minDate = new Date("1930-01-01");
 
-		const msToTime =(s) => {
-            if(this.state.counter === true){
-                return "0:0:0:0:0"
-            }
+		const msToTime = (s) => {
+			if (this.state.counter === true) {
+				return "0:0:0:0:0";
+			}
 			var ms = s % 1000;
 			s = (s - ms) / 1000; //seconds
 			var secs = s % 60;
 			s = (s - secs) / 60; //minutes
 			var mins = s % 60;
 			s = (s - mins) / 60; // hours
+			console.log(s);
 			var hrs = s % 24;
-            s = (s - hrs) / 24; //days
-            var day = s % 365;
-            s = (s - day) / 365 // years
-
+			s = (s - hrs) / 24; //days
+			var day = s % 365;
+			s = (s - day) / 365; // years
 			var years = Math.floor(s);
 
-			return "Your Age is " + years + " years, " + day + "days,  " + hrs + "hours, " + mins + "mins, " + secs + "seconds"; //+ "." + ms
-		}
+			if (years === 0) {
+				return (
+					"Your Age is " +
+					day +
+					"days, " +
+					hrs +
+					"hours, " +
+					mins +
+					"mins, " +
+					secs +
+					"seconds"
+				); //+ "." + ms
+			}
 
-        var timeToHour = msToTime(time);
+			return (
+				"Your Age is " +
+				years +
+				" years, " +
+				day +
+				"days, " +
+				hrs +
+				"hours, " +
+				mins +
+				"mins, " +
+				secs +
+				"seconds"
+			); //+ "." + ms
+		};
 
-        console.log("====================================");
-		console.log(this.state.counter);
-		console.log("====================================");
+		var timeToHour = msToTime(time);
 
 		return (
 			<div className="Age">
-				<h1>Test Age</h1>
+				<h1>Your Age</h1>
 				<DatePicker
 					selected={this.state.startDate}
 					onChange={this.handleChange}
